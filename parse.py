6963b9a8cc868b7a0b963a75c7275e2818917195
Aaron Goldstein <aaronmgoldstein@gmail.com>
2020-04-16 15:18:16 -0700

    Implemented a really sloppy way of finding nets

--- parse.py
+++ parse.py
@@ -76,58 +76,79 @@ def determine_parts_list(schematic):
 
 def determine_net_list(schematic):
     wires = [ record for record in schematic["records"] if record["RECORD"] == "27" ]
-    contacts = [ record for record in schematic["records"] if record["RECORD"] == "2" ]
-    
-    wire_groups = []
     
     p = re.compile('^(?P<prefix>X)(?P<index>\d+)$')
     for wire in wires:
         coord_name_matches = [x for x in [p.match(key) for key in wire.keys()] if x]
         wire['coords'] = [ ( int(wire['X' + match.group('index')]) , int(wire['Y' + match.group('index')]) )
                            for match in coord_name_matches ]
-
-    group_counter = 0
-    for wire_a in wires:
-        other_wires = [wire for wire in wires if wire["index"] != wire_a["index"]]
-        wire_a_vertex_pairs = [(wire_a['coords'][i], wire_a['coords'][i + 1]) for i in
-                               range(len(wire_a['coords']) - 1)]
-        
-        for wire_b in other_wires:
-            wire_b_vertex_pairs = [(wire_b['coords'][i], wire_b['coords'][i + 1]) for i in
-                                   range(len(wire_b['coords']) - 1)]
-            
-            for vertex_a_pair in wire_a_vertex_pairs:
-                for vertex_b_pair in wire_b_vertex_pairs:
-                    if ( (( vertex_a_pair[0][0] <= vertex_b_pair[0][0] <= vertex_a_pair[1][0] )
-                                and ( vertex_a_pair[0][1] <= vertex_b_pair[0][1] <= vertex_a_pair[1][1] ))
-                            or (( vertex_a_pair[0][0] >= vertex_b_pair[0][0] >= vertex_a_pair[1][0] )
-                                and ( vertex_a_pair[0][1] >= vertex_b_pair[0][1] >= vertex_a_pair[1][1] ))
-                            or (( vertex_a_pair[0][0] <= vertex_b_pair[1][0] <= vertex_a_pair[1][0] )
-                                and ( vertex_a_pair[0][1] <= vertex_b_pair[1][1] <= vertex_a_pair[1][1] ))
-                            or (( vertex_a_pair[0][0] >= vertex_b_pair[1][0] >= vertex_a_pair[1][0] )
-                                and ( vertex_a_pair[0][1] >= vertex_b_pair[1][1] >= vertex_a_pair[1][1] )) )\
-                        or ( (( vertex_b_pair[0][0] <= vertex_a_pair[0][0] <= vertex_b_pair[1][0] )
-                                and ( vertex_b_pair[0][1] <= vertex_a_pair[0][1] <= vertex_b_pair[1][1] ))
-                            or (( vertex_b_pair[0][0] >= vertex_a_pair[0][0] >= vertex_b_pair[1][0] )
-                                and ( vertex_b_pair[0][1] >= vertex_a_pair[0][1] >= vertex_b_pair[1][1] ))
-                            or (( vertex_b_pair[0][0] <= vertex_a_pair[1][0] <= vertex_b_pair[1][0] )
-                                and ( vertex_b_pair[0][1] <= vertex_a_pair[1][1] <= vertex_b_pair[1][1] ))
-                            or (( vertex_b_pair[0][0] >= vertex_a_pair[1][0] >= vertex_b_pair[1][0] )
-                                and ( vertex_b_pair[0][1] >= vertex_a_pair[1][1] >= vertex_b_pair[1][1] )) ):
-                        print("line {0} intersects with line {1}".format(vertex_a_pair, vertex_b_pair))
-                        
-                        
-                        
-                        if ( wire_a.get('group', None) != None ) and ( wire_b.get('group', None) == None ):
-                            wire_b['group'] = wire_a['group']
-                        elif (wire_a.get('group', None) == None) and (wire_b.get('group', None) != None):
-                            wire_a['group'] = wire_b['group']
-                        else:
-                            wire_a['group'] = group_counter
-                            wire_b['group'] = group_counter
-                            group_counter += 1
-                        
+    
+    nets = []
+    for wire in wires:
+        if wire["index"] not in [id for net in nets for id in net]:
+            nets.append(find_connected_wires(wire, [], schematic))
+    
+    schematic["nets"] = nets
+    
     return schematic
+
+def find_connected_wires(wire, visited, schematic):
+    neighbors = find_neighbors(wire, schematic)
+    print('entering: {0}'.format(wire['index']))
+    
+    if wire['index'] not in visited:
+        print('adding: {0} to {1}'.format(wire['index'], visited))
+        visited.append(wire['index'])
+        
+        for neighbor in neighbors:
+            print('trying: {0} of {1}'.format(neighbor['index'], [x['index'] for x in neighbors]))
+            visited = find_connected_wires(neighbor, visited, schematic)
+            print('visited = {0}'.format(visited))
+    else:
+        print('skipping: {0} already in list {1}'.format(wire['index'], visited))
+    
+    print('returning: {0}'.format(wire['index']))
+    return visited
+
+def find_neighbors(wire, schematic):
+    all_wires = [record for record in schematic["records"] if record["RECORD"] == "27"]
+    other_wires = [record for record in all_wires if record != wire]
+    
+    neighbors = []
+    for other_wire in other_wires:
+        if is_connected(wire, other_wire):
+            neighbors.append(other_wire)
+    
+    return neighbors
+
+def is_connected(wire_a, wire_b):
+    a_vertex_pairs = [(wire_a['coords'][i], wire_a['coords'][i + 1]) for i in
+                      range(len(wire_a['coords']) - 1)]
+    b_vertex_pairs = [(wire_b['coords'][i], wire_b['coords'][i + 1]) for i in
+                      range(len(wire_b['coords']) - 1)]
+    
+    for a_vx_pair in a_vertex_pairs:
+        for b_vx_pair in b_vertex_pairs:
+            if (((a_vx_pair[0][0] <= b_vx_pair[0][0] <= a_vx_pair[1][0])
+                 and (a_vx_pair[0][1] <= b_vx_pair[0][1] <= a_vx_pair[1][1]))
+                or ((a_vx_pair[0][0] >= b_vx_pair[0][0] >= a_vx_pair[1][0])
+                    and (a_vx_pair[0][1] >= b_vx_pair[0][1] >= a_vx_pair[1][1]))
+                or ((a_vx_pair[0][0] <= b_vx_pair[1][0] <= a_vx_pair[1][0])
+                    and (a_vx_pair[0][1] <= b_vx_pair[1][1] <= a_vx_pair[1][1]))
+                or ((a_vx_pair[0][0] >= b_vx_pair[1][0] >= a_vx_pair[1][0])
+                    and (a_vx_pair[0][1] >= b_vx_pair[1][1] >= a_vx_pair[1][1])))\
+                or (((b_vx_pair[0][0] <= a_vx_pair[0][0] <= b_vx_pair[1][0])
+                 and (b_vx_pair[0][1] <= a_vx_pair[0][1] <= b_vx_pair[1][1]))
+                or ((b_vx_pair[0][0] >= a_vx_pair[0][0] >= b_vx_pair[1][0])
+                    and (b_vx_pair[0][1] >= a_vx_pair[0][1] >= b_vx_pair[1][1]))
+                or ((b_vx_pair[0][0] <= a_vx_pair[1][0] <= b_vx_pair[1][0])
+                    and (b_vx_pair[0][1] <= a_vx_pair[1][1] <= b_vx_pair[1][1]))
+                or ((b_vx_pair[0][0] >= a_vx_pair[1][0] >= b_vx_pair[1][0])
+                    and (b_vx_pair[0][1] >= a_vx_pair[1][1] >= b_vx_pair[1][1]))):
+                
+                return True
+    
+    return False
 
 def main(args):
     schematic = parse(**vars(args))
